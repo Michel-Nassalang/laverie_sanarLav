dc0ad7d4f36f5d0327a3278dbdbb2936ae888928
Michel Nassalang <68905963+Michel-Nassalang@users.noreply.github.com>
2023-06-05 18:09:10 +0000

    Conception de la partie front

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route('/', name: 'app_main')]
+    public function main(): Response
+    {
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+    #[Route('/default', name: 'app_default')]
+    public function index(): Response
+    {
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+}
